3945f7a50f9263a9b89e9d123a0d79c50374d0b3
Sebastian Flügge <sebastian.fluegge@faro.com>
2023-05-28 15:48:37 +0200

    Use Poco's ServerApplication for base station
    Refactor base_station main to make use of Poco's ServerApplication and support command line option parsing. Add options for setting ip address and grpc port.

--- src/base_station/main.cpp
+++ src/base_station/main.cpp
@@ -4,10 +4,6 @@
 
 #include <spdlog/spdlog.h>
 
-#include "absl/flags/flag.h"
-#include "absl/flags/parse.h"
-#include "absl/strings/str_format.h"
-
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
@@ -15,6 +11,11 @@
 #include <Poco/FileStream.h>
 #include <Poco/Net/DatagramSocket.h>
 #include <Poco/Net/SocketAddress.h>
+
+#include <Poco/Util/HelpFormatter.h>
+#include <Poco/Util/Option.h>
+#include <Poco/Util/OptionSet.h>
+#include <Poco/Util/ServerApplication.h>
 
 #include "ControlServiceImpl.h"
 #include "Logging.h"
@@ -35,35 +36,114 @@ using Poco::Net::SocketAddress;
 
 using namespace utils::logging;
 
-ABSL_FLAG(uint16_t, port, 50051, "Server port for the service");
-
 namespace base_station {
-void RunServer(uint16_t grpcPort)
-{
-    std::string server_address = absl::StrFormat("0.0.0.0:%d", grpcPort);
-    uint32_t maxConnections = 2;
-    uint32_t maxClients = 2;
-    auto connectionPool = std::make_unique<ConnectionPool>(maxConnections, maxClients);
-    ControlServiceImpl service(std::move(connectionPool));
 
-    grpc::EnableDefaultHealthCheckService(true);
-    grpc::reflection::InitProtoReflectionServerBuilderPlugin();
-    ServerBuilder builder;
-    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service);
-    std::unique_ptr<Server> server(builder.BuildAndStart());
-    spdlog::get("console")->info("Server listening on {}", server_address);
-    std::thread grpcServer([&] { server->Wait(); });
-    grpcServer.join();
-}
+class BaseStation : public Poco::Util::ServerApplication {
+  public:
+    BaseStation() {}
+
+  protected:
+    void initialize(Application& self) override
+    {
+        loadConfiguration(); // Load configuration files, if any
+        Poco::Util::ServerApplication::initialize(self);
+    }
+
+    void uninitialize() override { Poco::Util::ServerApplication::uninitialize(); }
+
+    void defineOptions(Poco::Util::OptionSet& options) override
+    {
+        Poco::Util::ServerApplication::defineOptions(options);
+
+        options.addOption(
+            Poco::Util::Option("help", "h")
+                .required(false)
+                .repeatable(false)
+                .description("Display help information")
+                .callback(Poco::Util::OptionCallback<BaseStation>(this, &BaseStation::handleHelp)));
+
+        options.addOption(
+            Poco::Util::Option("ip-address", "i")
+                .required(false)
+                .repeatable(false)
+                .argument("ip")
+                .binding("ip")
+                .description("IP address")
+                .callback(
+                    Poco::Util::OptionCallback<BaseStation>(this, &BaseStation::handleIpAddress)));
+
+        options.addOption(
+            Poco::Util::Option("grpc-port", "p")
+                .required(false)
+                .repeatable(false)
+                .argument("grpcPort")
+                .binding("grpcPort")
+                .description("IP address")
+                .callback(
+                    Poco::Util::OptionCallback<BaseStation>(this, &BaseStation::handleGrpcPort)));
+    }
+
+    void handleHelp([[maybe_unused]] const std::string& name,
+                    [[maybe_unused]] const std::string& value)
+    {
+        displayHelp();
+        stopOptionsProcessing();
+        exit(Poco::Util::Application::EXIT_OK);
+    }
+
+    void handleIpAddress([[maybe_unused]] const std::string& name, const std::string& value)
+    {
+        _ip = value;
+    }
+    void handleGrpcPort([[maybe_unused]] const std::string& name, const std::string& value)
+    {
+        _grpcPort = std::stoi(value);
+    }
+
+    std::string serverAddress() { return std::format("{}:{}", _ip, _grpcPort); }
+
+    void displayHelp()
+    {
+        Poco::Util::HelpFormatter helpFormatter(options());
+        helpFormatter.setCommand(commandName());
+        helpFormatter.setUsage("OPTIONS");
+        helpFormatter.setHeader("A server application for the base station.");
+        helpFormatter.format(std::cout);
+    }
+
+    int main([[maybe_unused]] const std::vector<std::string>& args) override
+    {
+        LOG_INFO("IP address: {}", _ip);
+        LOG_INFO("GrpcPort: {}", _grpcPort);
+
+        uint32_t maxConnections = 2;
+        uint32_t maxClients = 2;
+        auto connectionPool = std::make_unique<ConnectionPool>(maxConnections, maxClients);
+        ControlServiceImpl service(std::move(connectionPool));
+
+        grpc::EnableDefaultHealthCheckService(true);
+        grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+        ServerBuilder builder;
+        builder.AddListeningPort(serverAddress(), grpc::InsecureServerCredentials())
+            .RegisterService(&service);
+        std::unique_ptr<Server> server(builder.BuildAndStart());
+        spdlog::get("console")->info("Server listening on {}", serverAddress());
+        std::thread grpcServer([&] { server->Wait(); });
+        grpcServer.join();
+
+        return Poco::Util::ServerApplication::EXIT_OK;
+    }
+
+  private:
+    std::string _ip = "127.0.0.1";
+    int _grpcPort = 50051;
+};
+
 } // namespace base_station
 
-using base_station::RunServer;
-
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-    absl::ParseCommandLine(argc, argv);
     initializeLogger();
-    RunServer(absl::GetFlag(FLAGS_port));
-    return 0;
+    base_station::BaseStation app;
+    return app.run(argc, argv);
 }
